81ff65aab31ec75c31605447de3da657e1f4e05a
AlekseyPauls <aleksey.pauls@mail.ru>
2018-08-04 16:22:04 +0700

    test

--- SlackBot/app.py
+++ SlackBot/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request
-from flask import make_response
+from flask import make_response, Response
 import os, json
 
 app = Flask(__name__)
@@ -13,6 +13,25 @@ def hello_slack():
     response = make_response(response_body)
     response.headers['Content-Type'] = 'application/json'
     return response
+
+
+SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
+
+
+@app.route('/slack', methods=['POST'])
+def inbound():
+    if request.form.get('token') == SLACK_WEBHOOK_SECRET:
+        channel = request.form.get('channel_name')
+        username = request.form.get('user_name')
+        text = request.form.get('text')
+        inbound_message = username + " in " + channel + " says: " + text
+        print(inbound_message)
+    return Response(), 200
+
+
+@app.route('/', methods=['GET'])
+def test():
+    return Response('It works!')
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
