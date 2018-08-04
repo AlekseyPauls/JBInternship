fe8901858c7ce32f8ea544a364df9f60c6ca8657
AlekseyPauls <aleksey.pauls@mail.ru>
2018-08-04 18:07:42 +0700

    test

--- app.py
+++ app.py
@@ -6,21 +6,21 @@ import os, json
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/webhook', methods=['POST', 'GET'])
 def hello_slack():
     print(request.data)
     slack_event = json.loads(request.data.decode("UTF-8"))
     print(slack_event)
     if "challenge" in slack_event:
         return make_response(slack_event["challenge"], 200, {"content_type": "application/json"})
-    else:
-        print("Error")
+    if "message.im" in slack_event:
+        return make_response("ok", 200, {"content_type": "application/json"})
 
 
 SLACK_WEBHOOK_SECRET = "xoxb-409605691809-409444378032-ktcVNWRxS8ziKtnkNnpWV1qW"
 
 
-@app.route('/slack', methods=['POST'])
+@app.route('/slack', methods=['POST', 'GET'])
 def inbound():
     if request.form.get('token') == SLACK_WEBHOOK_SECRET:
         channel = request.form.get('channel_name')
